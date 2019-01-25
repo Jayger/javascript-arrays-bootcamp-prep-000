32d03017beed8b9566d5da4d15bc913b4c528d38
Jordan Rivera <jordan.rivera34@gmail.com>
2019-01-25 06:52:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element) {
-  array = [element, ...array];
-  return array;
-}
+w
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
   return array;
 }
