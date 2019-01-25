d44d7455569714a614832918f08ba54a1fc30017
Jordan Rivera <jordan.rivera34@gmail.com>
2019-01-25 07:29:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function ElemenremovetFromBeginningOfArray(array) {
-  return array.slice();
+  array.slice();
+  return array;
 
 } 
 
