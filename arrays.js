99414657bda8fde4f383815b3a53c1e15b8e8602
Jordan Rivera <jordan.rivera34@gmail.com>
2019-01-25 07:00:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array = [...array, element];
+  array = [1, element];
   return array;
 }
 
 function addElementToEndOfArray(array, element) {
-  array = [element, ...array];
+  array = [element, 1];
   return array;
 }
 
